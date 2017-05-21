4a1b3e999f85cc3602488257774f05adc0b58c11
wanglianghai <1540446221@qq.com>
2017-05-21 15:19:48 +0800

    handler创建在哪是哪的looper

--- app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
@@ -1,7 +1,16 @@
 package com.bignerdranch.android.photogallery;
 
+import android.content.pm.ProviderInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by Administrator on 2017/5/21/021.
@@ -9,10 +18,31 @@ import android.util.Log;
 //identify each download and to determine which UI element to update
 public class ThumbnailDownloader<T> extends HandlerThread {
     private static final String TAG = "ThumbnailDownloader";
+    private static final int MESSAGE_DOWNLOAD = 1;
+
+    private Handler mRequestHandler;
+    private ConcurrentMap<T, String> mRequestMap;
 
     private boolean mHasQuit = false;
     public ThumbnailDownloader() {
         super(TAG);
+        mRequestMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    protected void onLooperPrepared() {
+        super.onLooperPrepared();
+        mRequestHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if (msg.what == MESSAGE_DOWNLOAD) {
+                    T target = (T) msg.obj;
+                    Log.i(TAG, "handleMessage: getUrl" + mRequestMap.get(target));
+                    handlerRequest(target);
+                }
+            }
+        };
     }
 
     @Override
@@ -23,5 +53,25 @@ public class ThumbnailDownloader<T> extends HandlerThread {
 
     public void queueThumbnail(T target, String url) {
         Log.i(TAG, "get a url: " + url);
+
+        if (url == null) {
+            mRequestMap.remove(target);
+        } else {
+            mRequestMap.put(target, url);
+            mRequestHandler.obtainMessage(MESSAGE_DOWNLOAD, target)
+                    .sendToTarget();
+        }
+    }
+
+    private void handlerRequest(T target) {
+        String url = mRequestMap.get(target);
+        
+        try {
+            byte[] bitImg = new PhotoFetcher().getUrlBytes(url);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(bitImg, 0, bitImg.length);
+            Log.i(TAG, "handlerRequest: bitmap create");
+        } catch (IOException e) {
+            Log.e(TAG, "handlerRequest: Error download image", e);
+        }
     }
 }
