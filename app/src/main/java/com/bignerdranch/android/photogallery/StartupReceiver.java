93cd76b9166510b5cd6b24f9ae77577f5d0a390c
wanglianghai <1540446221@qq.com>
2017-06-03 16:04:58 +0800

    standalone receiver

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/StartupReceiver.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.android.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/6/3/003.
+ */
+
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "onReceive: Received broadcast intent " + intent.getAction());
+    }
+}
